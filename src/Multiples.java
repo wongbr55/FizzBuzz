27236e9048a5c13be0e26b12a4251149c11c796c
Brandon <sudburymailing@gmail.com>
2023-09-11 14:25:10 -0400

    Fixed Multiples

--- src/Multiples.java
+++ src/Multiples.java
@@ -2,11 +2,13 @@ public class Multiples {
 
     public static void main(String[] args){
 
+        int counter = 0;
         for (int i = 1; i < 1000; i++){
             if (i % 3 == 0 || i % 5 == 0){
-                System.out.println(i);
+                counter++;
             }
         }
+        System.out.println(counter);
 
     }
 }
