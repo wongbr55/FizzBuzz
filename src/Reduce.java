bff78c1bea309c3b608cff6379d98ba908a0422f
Brandon <sudburymailing@gmail.com>
2023-09-11 14:10:47 -0400

    Created and implimented Reduce file

---
+++ src/Reduce.java
@@ -0,0 +1,18 @@
+public class Reduce {
+
+    public static void main(String [] args){
+
+        int counter = 0;
+        int n = 100;
+        while (n != 0){
+            if (n % 2 == 0){
+                n = n / 2;
+            }
+            else{
+                n = n - 1;
+            }
+            counter++;
+        }
+        System.out.println(counter);
+    }
+}
