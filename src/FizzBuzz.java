ae03841e8ae73c26af25050c19eab59309f4c282
Brandon <sudburymailing@gmail.com>
2023-09-11 13:56:03 -0400

    Created a comment

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,6 +5,7 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
+        // This is a comment
         int i = 1;
         while (i < 100) {
 
@@ -14,7 +15,7 @@ class FizzBuzz {
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
