866e059938f157a2a2df5ea68f82b0b59acbe937
Brandon <sudburymailing@gmail.com>
2023-09-11 14:05:00 -0400

    Created Multiples class

---
+++ src/Multiples.java
@@ -0,0 +1,12 @@
+public class Multiples {
+
+    public static void main(String[] args){
+
+        for (int i = 1; i < 1000; i++){
+            if (i % 3 == 0 || i % 5 == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
